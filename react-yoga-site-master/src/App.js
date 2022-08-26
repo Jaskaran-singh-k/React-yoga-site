8f0ac48f06fc4d51fa7ef4259ad1e4f7cec1d6c8
jas kohli <jaskaran.1p@gmail.com>
2022-08-26 16:20:22 +0530

    react yoga site

---
+++ react-yoga-site-master/src/App.js
@@ -0,0 +1,25 @@
+npm import { BrowserRouter, Route } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import ClassDetails from "./Pages/ClassDetails/ClassDetails";
+import Homepage from "./Pages/Homepage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+
+        <Route path="/" exact>
+          <Homepage />
+        </Route>
+        <Route path="/class/:id" component={ClassDetails} />
+
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
